31c55fcf3e42ffe481011d3890ddeff864775d6f
Alexis Mora <alexis.mora.sanchez@gmail.com>
2022-08-27 22:37:44 +0200

    feat: added material-ui + started on navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,19 +1,25 @@
-import React from "react"
-import Link from "next/link"
+import * as React from 'react'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
 
-const Navbar = () => {
+import { Avocado } from '@components/SVGIcons'
+
+export default function ButtonAppBar() {
   return (
-    <nav>
-     <menu>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-     </menu>
-    </nav>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Avocado />
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Avocado Store
+          </Typography>
+          {/* TODO: Shopping Cart */}
+          <Button color="inherit">Shopping Cart</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   )
 }
-
-export default Navbar
