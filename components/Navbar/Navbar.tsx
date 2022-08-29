732a2e1caa821712a9ac201521e5c00af1cffc18
Alexis Mora <alexis.mora.sanchez@gmail.com>
2022-08-29 22:16:29 +0200

    WIP: navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -5,23 +5,26 @@ import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
 
-import { Avocado } from '@components/SVGIcons'
+import { Avocado, Basket } from '@components/SVGIcons'
 import { useRouter } from 'next/router'
 
 export default function ButtonAppBar() {
   const router = useRouter()
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="fixed">
+      <AppBar position="fixed" style={{background: '#FFFFFF'}}>
         <Toolbar>
           <Button onClick={()=> router.push('/') } color="inherit">
             <Avocado />
           </Button>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} color="black">
             Avocado Store
           </Typography>
-          {/* TODO: Shopping Cart */}
-          <Button color="inherit">Shopping Cart</Button>
+          {/* TODO: Functional Shopping Cart */}
+          <Button  style={{color: '#000', textTransform: 'none'}}>
+            <Basket />
+            Shopping Cart (0)
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
